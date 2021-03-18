9c25bd9aa208be16664a37f04decd6b1da3f23c0
Flavien Ropers <ropersflavien@gmail.com>
2021-03-18 18:15:03 +0100

    Modication suite à soutenance

--- frontend/js/Model.js
+++ frontend/js/Model.js
@@ -1,40 +1,25 @@
 class Model {
     /* Récupère le contenu de l'api */
     static ajaxGet(apiUrl) {
-        return new Promise(function(resolve, reject) {
-            let xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === xhr.DONE) {
-                    if (xhr.status === 200) { 
-                        let content = JSON.parse(xhr.responseText)
-                        resolve(content);
-                    } else {
-                        reject(xhr);
-                    } 
-                }
-            }
-            xhr.open('GET', apiUrl, true);
-            xhr.send();
-        });
+        return fetch(apiUrl)
+            .then(response => response.json())
+            .catch((error) => {
+                console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+            });
     }
 
     /* Envoie le contenu du panier (formulaire et produit) et récupère la réponse de l'api */
     static ajaxPost(apiUrl, contact, products) {
-        return new Promise(function(resolve, reject) {
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", apiUrl, true);
-            xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.send(JSON.stringify({contact, products}));
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === xhr.DONE) {
-                    if (xhr.status === 201) { 
-                        let content = (xhr.responseText)
-                        resolve(content);
-                    } else {
-                        reject(xhr);
-                    } 
-                }
-            }
+        return fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ contact, products })
+        })
+        .then(response => response.json())
+        .catch((error) => {
+            console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
         });
     }
 }
